a7b6af0f8e00d8e048381b8bcca33726f0d12895
Godfrey Onyango <godiodhis268@gmail.com>
2024-07-09 16:09:15 +0300

    last

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const removingMain=document.getElementById('main');
+removingMain.remove();
+
+const newHeader=document.createElement('h1');
+newHeader.id="victory";
+newHeader.innerHTML='Godi is the champion';
